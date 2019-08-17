5149bdc797629fd6ee93b052d2a830c10a49ba17
imlegend19 <mahengandhi19@gmail.com>
2019-08-17 20:28:03 +0530

    modified administration

--- drf_user/variables.py
+++ drf_user/variables.py
@@ -1,10 +1,3 @@
-"""
-All static variables used in the system.
-
-Author: Himanshu Shankar (https://himanshus.com)
-Author: Aditya Gupta (https://github.com/ag93999)
-"""
-
 EMAIL = "E"
 MOBILE = "M"
 DESTINATION_CHOICES = [

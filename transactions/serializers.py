35e2cfe724f8e183a6a3f82ee69c6e6550d1f6d1
imlegend19 <mahengandhi19@gmail.com>
2019-08-21 18:42:53 +0530

    now showing serializer in transaction

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 
 
 class OrderPaymentSerializer(serializers.ModelSerializer):
+    from employee.serializers import EmployeeSerializer
+
+    accepted_by = EmployeeSerializer(many=False, read_only=True)
+
     class Meta:
         from .models import OrderPayment
 
